4ead4678c7d8744c68cecc56a4d5e4ff4b718368
local / BlogBooks <77278342+Nobody-Local@users.noreply.github.com>
2022-12-16 22:31:36 +0900

    Fix Start Line: fix => const target

---
+++ assets/functions.js
@@ -0,0 +1,42 @@
+const BlogBooks = {
+    posts() {
+        fetch("https://blogbooks-fetcher-1.haru070.repl.co/posts")
+            .then(response => response.json())
+            .then(query => {
+                var $content;
+                query.forEach(data => {
+                    if (data.excerpt.protected == "true") {} else {
+                        $content += `<div class="post" data-postID="${data.id}"><div class="title">${data.title.rendered}</div><div class="description">${data.excerpt.rendered}</div></div>`;
+                    }
+                });
+                document.body.innerHTML = $content;
+            })
+            .catch(e => {
+                document.body.innerHTML = e;
+                console.error(e);
+            })
+    },
+    page(id = "") {
+        if (id === (null || "" || undefined)) return;
+        fetch(`https://blogbooks-fetcher-1.haru070.repl.co/page/?id=${id}`)
+            .then(response => response.json())
+            .then(query => {
+                $content = `<h1>${query.title.rendered}</h1><div class="date">Date: ${query.date}</div><div class="content">${query.content.rendered}</div>`;
+                document.body.innerHTML = $content;
+            })
+            .catch(e => {
+                document.body.innerHTML = e;
+                console.error(e);
+            })
+    }
+}
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
